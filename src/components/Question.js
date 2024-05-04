979677c6698632cf879d869de8df6feb6d79459d
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-05-04 16:33:03 +0300

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from "react";
+import React, {useEffect , useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
